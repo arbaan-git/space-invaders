cf884a5e2a69bb8b1e11570117d3107c769b5d0a
arbaan-git <mohammed.arbaan2@gmail.com>
2025-06-06 15:35:23 +0530

    Add player movement left to right and remove background

--- space-invaders.py
+++ space-invaders.py
@@ -2,7 +2,6 @@ import os
 import pygame
 from sys import exit
 
-os.chdir("D:/coding/pygame/space-invaders")
 pygame.init()
 
 
@@ -13,15 +12,20 @@ pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 font = pygame.font.Font("graphics/JockeyOne-Regular.ttf", 25)
 
-background_surface = pygame.image.load("graphics/background.png").convert_alpha()
-header_text_surface = font.render("Space Invaders: A practice project", False, "White")
+background_surf = pygame.surface.Surface((512, 512))
+header_text_surf = font.render("Space Invaders: A practice project", False, "White")
+header_text_rect = header_text_surf.get_rect(center = (screen_width/2, 10))
 
-player_surface = pygame.image.load("graphics/player.png").convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (screen_width/2, screen_height - 100))
-player_x_position = 0
+player_surf = pygame.image.load("graphics/player.png").convert_alpha()
+player_x_pos = 0
+player_rect = player_surf.get_rect(midbottom=(player_x_pos, screen_height - 100))
 
-monster_surface = pygame.image.load('graphics/green.png').convert_alpha()
-monster_rect = monster_surface.get_rect(midbottom = (screen_width/2, 100))
+monster_surf = pygame.image.load("graphics/green.png").convert_alpha()
+monster_rect = monster_surf.get_rect(midbottom=(screen_width / 2, 100))
+
+bullet_surf = pygame.Surface((10, 10))
+bullet_surf.fill("Yellow")
+bullet_rect = bullet_surf.get_rect(midbottom=player_rect.midtop)
 
 
 while True:
@@ -29,11 +33,22 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
-    screen.blit(background_surface, (0, 0))
-    screen.blit(header_text_surface, (10, 10))
-    screen.blit(player_surface, player_rect)
-    screen.blit(monster_surface, monster_rect)
+            
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_rect.x -= 10
+                bullet_rect.midbottom = player_rect.midtop
+            if event.key == pygame.K_RIGHT:
+                player_rect.x+= 10
+                bullet_rect.midbottom = player_rect.midtop
+                
+                
+    screen.blit(background_surf, (0, 0))
+    screen.blit(header_text_surf, header_text_rect)
+    screen.blit(player_surf, player_rect)
+    screen.blit(monster_surf, monster_rect)
+    screen.blit(bullet_surf, bullet_rect)
 
     pygame.display.update()
     clock.tick(60)
